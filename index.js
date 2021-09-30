c73eb4761759a714aa165ac1ce9e38eee848ebe8
Hagen <hagen@paw.bot>
2021-09-30 21:41:01 +0100

    remove unnecessary deps, better parsing

--- index.js
+++ index.js
@@ -2,33 +2,76 @@
 
 'use strict';
 
-const parser = require('mri');
+const mri = require('mri');
+const chalk = require('chalk');
+const nodePath = require('path');
 const merge = require('fivem-merge');
-const args = require('args')
-  .option('output', 'The merge destination')
-  .option('verbose', 'Enable verbose logging', false)
-  .option('lint', 'Lint XML output to be easily readable', true)
-  .example('vmerge ./bmw3 ./bmw4 ./18bmw --output ./bmws', 'Merge bmw3, bmw4 and 18bmw into bmws')
+const fs = require('fs');
 
-const parsed = args.parse(process.argv, {
-  name: 'vmerge'
-})
+const parsed = mri(process.argv.slice(2))
 
+if ((parsed.version ?? parsed.v) || parsed._[0] === 'version') {
+  console.log(`cli v${require('./package.json').version}`);
+  console.log(`fivem-merge v${require(nodePath.join(require.resolve('fivem-merge'), '..', '..', 'package.json')).version}`);
 
-const paths = parser(process.argv.slice(2))._;
-
-if (paths.length > 0) {
-  
+  process.exit();
 }
 
-if (!parsed.output) {
-  console.error('  --output is a required parameter')
-  args.showHelp()
-  process.exit()
+if ((parsed.help ?? parsed.h) || parsed._[0] === 'help') {
+  console.log(`  Usage:`);
+  console.log(`    ${chalk.yellow('vmerge')} <...paths> -o <path>  Merge two or more resources and output them at ${chalk.whiteBright('path')}`);
+  console.log(`    ${chalk.yellow('vmerge help')}                  This.`);
+  console.log(`    ${chalk.yellow('vmerge version')}               The version of this package.`);
+  console.log();
+  console.log(`  Options:`);
+  console.log(`    ${chalk.yellow('--output , -o')}  The merge destination.`);
+  console.log(`    ${chalk.yellow('--help   , -h')}  This.`);
+  console.log(`    ${chalk.yellow('--verbose, -v')}  Enable additional info.`);
+  console.log(`    ${chalk.yellow('--no-lint, -l')}  Enable this to disable meta linting.`);
+  console.log();
+  console.log(`  Examples:`);
+  console.log(`    ${chalk.yellow('- Merge bmw3, bmw4 and 18bmw into bmws')}`);
+  console.log(`    vmerge ./bmw3 ./bmw4 ./18bmw ${chalk.grey('--output')} ./bmws`);
+
+  process.exit();
+}
+
+const paths = [...parsed._];
+function hmBool(name) {
+  if (name in parsed) {
+    if (typeof parsed[name] === 'string') paths.push(parsed[name]);
+    return true;
+  }
+  return false;
+}
+const noLint = hmBool('no-lint') ?? hmBool('l') ?? false;
+const verb = hmBool('verbose') ?? hmBool('v') ?? false;
+const output = parsed.output ?? parsed.o;
+
+if (!output) {
+  console.warn(`The ${chalk.grey('--output')} option is required.`);
+  process.exit();
+}
+
+if (!(0 in paths)) {
+  console.warn(`Please supply some paths to resources to merge.`);
+  process.exit();
+}
+
+if (fs.existsSync(output)) {
+  console.warn(`${chalk.grey('fivem-merge')} cannot output files into an already existing directory.`);
+  process.exit();
+}
+
+for (const path of paths) {
+  if (!fs.existsSync(path)) {
+    console.warn(`The resource ${chalk.grey(path)} does not exist.`);
+    process.exit();
+  }
 }
 
 merge(paths, {
-  verbose: parsed.verbose,
-  outputPath: parsed.output,
-  lintOutput: parsed.lint,
+  verbose: verb,
+  outputPath: output,
+  lintOutput: !noLint,
 })
